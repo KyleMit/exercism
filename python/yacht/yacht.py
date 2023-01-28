7a1db4e62fc90a9a5e3d90681a06bb55917b1470
KyleMit <kmitofsky@stackoverflow.com>
2023-01-28 08:49:52 -0500

    solve yacht

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 # Score categories.
 # Change the values as you see fit.
 YACHT = 1
@@ -30,7 +32,46 @@ def score(dice, category):
     if category == SIXES:
         return sum([i for i in dice if i == 6])
     if category == FULL_HOUSE:
-        return sum(dice) if (len(set(dice))) == 2 else 0
+        cnt = Counter(dice)
+        if len(cnt) != 2:
+            return 0
+        first, second = cnt.most_common(2)
+        return sum(dice) if first[1] == 3 and second[1] == 2 else 0
     if category == CHOICE:
         return sum(dice)
+    if category == FOUR_OF_A_KIND:
+        cnt = Counter(dice)
+        die, qty = cnt.most_common()[0]
+        return die * 4 if qty >= 4 else 0
+    if category == LITTLE_STRAIGHT:
+        return (
+            30
+            if longestConsecutiveSubsequence(dice) == 5 and sorted(dice)[0] == 1
+            else 0
+        )
+    if category == BIG_STRAIGHT:
+        return (
+            30
+            if longestConsecutiveSubsequence(dice) == 5 and sorted(dice)[0] == 2
+            else 0
+        )
     return 0
+
+
+def longestConsecutiveSubsequence(xs):
+    uniques = list(set(sorted(xs)))
+
+    longestRun = 0
+    currentRun = 0
+
+    for i, el in enumerate(uniques):
+        if i == 0:
+            currentRun = 1
+        elif el == uniques[i - 1] + 1:
+            currentRun += 1
+        else:
+            currentRun = 1
+
+        longestRun = max(longestRun, currentRun)
+
+    return longestRun
